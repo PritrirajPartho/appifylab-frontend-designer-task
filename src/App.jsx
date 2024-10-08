b4e2025383aee873c0c9a10b02c847fef3dabe65
Pritiraj Partho <dpartho5616@gmail.com>
2024-10-08 15:37:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import  React  from 'react'
+
+function App() {
+
+  return (
+   <div>
+      <h1 className='text-red-700 text-center text-4xl'>Task from appifylab</h1>
+   </div>
+  )
+}
+
+export default App
